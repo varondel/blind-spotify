084e1d9b73736c8ea8b19f79795c41bd6c41cfaa
varondel <arondelv@gmail.com>
2018-06-04 23:45:40 +0200

    play sound

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
+import Sound from 'react-sound';
 
-const tokenAPI = 'BQAHKW0x0ztAf6vXu2OHLIUN5_Fn8psWhYtuUmme_wn_Fe44p_s5JB6EtT58jgKyK3Z0qWKXrX5WLbVEb4dbjTKNmdmx-CrQ6L_VsrzmOZ5YM4q0k94OPOtqXQf2wt5pFfcgb1v4_LKT4wNQHqKP';
+//const tokenAPI = 'BQAHKW0x0ztAf6vXu2OHLIUN5_Fn8psWhYtuUmme_wn_Fe44p_s5JB6EtT58jgKyK3Z0qWKXrX5WLbVEb4dbjTKNmdmx-CrQ6L_VsrzmOZ5YM4q0k94OPOtqXQf2wt5pFfcgb1v4_LKT4wNQHqKP';
+let response
 
 class TrackImg extends Component {
     
@@ -13,7 +15,8 @@ class TrackImg extends Component {
       if (!images || !images.length) return null;
   
       const src = images[0].url;
-      return <img src={src} style={{ width: 400, height: 400 }} />;
+      //eslint-disable-next-line
+      return <img src={src} alt="no image" style={{ width: 400, height: 400 }} />;
     }
   }
 
@@ -23,7 +26,7 @@ class App extends Component {
     super();
     this.state = {
       text: "Bonjour",
-      response: "",
+      response: {},
       index: 1,
       precedent: "None",
       following: "",
@@ -33,12 +36,20 @@ class App extends Component {
    
    
   componentDidMount() {
+    console.log("DidMount !!")
+
+    let parse = queryString.parse(window.location.search)
+    let accessToken = parse.access_token
+
+    if (!accessToken)
+      return
+
     fetch('https://api.spotify.com/v1/me/tracks', {
       method: 'GET',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + tokenAPI,
+        Authorization: 'Bearer ' + accessToken,
       },
     })
       .then(
@@ -46,7 +57,7 @@ class App extends Component {
       ) 
       .then((response) => {
         console.log(response)
-        this.setState({text: response.items.length + " Son chargés!", response: response })
+        this.setState({response: response })
         this._startNewSong()
        })
   }
@@ -57,25 +68,25 @@ class App extends Component {
     console.log(index)
     var currentTrack = this.state.response.items[index].track
     
-    
     var previewIndex = index > 0 ? index - 1 : this.state.response.total - 1
     this.setState({precedent: this.state.response.items[previewIndex].track.name})
     var followIndex = (index + 1) % this.state.response.total
     this.setState({following: this.state.response.items[followIndex].track.name})
     
-    this._playSound(currentTrack.preview_url);
+    //this._playSound(currentTrack.preview_url);
     console.log("playing " + currentTrack.name + " from " + currentTrack.artists[0].name)
   }
 
-  _playSound = (url) => {
-    this.audio = new Audio(url);
-    this.audio.play()
-  }
+  //_playSound = (url) => {
+  //  this.audio = new Audio(url);
+  //  this.audio.play()
+  //}
 
   render() {
     if (!this.state.response.total) { 
     //  return( <ActivityIndicator size="large" color="#0000ff" /> )
-      return("Waiting")
+      return(<button className="App"
+      onClick = {()=>{window.location='http://localhost:8888/login'}}> Sign in to spotify </button>)
     }
 
     const index = this.state.index
@@ -87,6 +98,10 @@ class App extends Component {
        <h3> { "following : " + this.state.following } </h3>
        <h3> { currentTrack.name } </h3>
        <TrackImg track = { currentTrack } />
+       <Sound
+          url = {this.state.response.items[this.state.index].track.preview_url}
+          playStatus={Sound.status.PLAYING}
+      />
      </div>
 );
   }
