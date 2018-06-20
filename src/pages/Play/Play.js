476a74f28da79dfee9517be9fec6654cdd478d14
varondel <arondelv@gmail.com>
2018-06-20 14:52:53 +0200

    change socket connection to heroku

--- src/pages/Play/Play.js
+++ src/pages/Play/Play.js
@@ -51,7 +51,7 @@ class App extends Component {
           trackList = response
           this.setState({gameState: "waitingPlayer"})
 
-          socket = openSocket('http://localhost:8888');
+          socket = openSocket('https://blind-spotify-backend.herokuapp.com:8888');
           console.log("Socket Id " + socket.id)
 
           // Listen if client is chosen to pick a song
