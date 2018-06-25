962baca3a924e7c918c4844fa36f11a7d0fc7b9a
varondel <arondelv@gmail.com>
2018-06-25 21:15:14 +0200

    isolate client and server

--- SocketServer.js
+++ SocketServer.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const socketIO = require('socket.io');
+const path = require('path');
 var http = require('http');
 
 var RoomUtils = require('./roomUtils');
@@ -8,9 +9,18 @@ var roomUtils = new RoomUtils()
 const PORT = process.env.PORT || 3006;
 
 let app = express()
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 var server = http.createServer(app)
 const io = require('socket.io').listen(server);
+
+// Put all API endpoints under '/api'
+app.get('/test', (req, res) => {
+  // Return them as json
+  res.json("TEST");
+
+  console.log(`TEST`);
+});
 
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
